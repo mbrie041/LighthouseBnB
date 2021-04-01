538057fd1530aa3228a2d8db61de5d12202da63d
Mike <mbrie041@gmail.com>
2021-04-01 18:35:20 +0000

    1_queries

---
+++ 1_queries/property_listing_by_city.sql
@@ -0,0 +1,15 @@
+-- SELECT properties.*, AVG(rating) as average_rating
+-- From properties;
+-- JOIN property_reviews ON property_id = properties.id
+-- WHERE city = 'vancouver'
+-- HAVING average_rating >= 4
+-- ORDER BY cost_per_night;
+
+SELECT properties.*, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
